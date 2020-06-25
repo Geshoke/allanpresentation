97ac276321b6d7e3c40a7ba0c64dbb927708ce78
Geshoke <eusebiogichuki@gmail.com>
2020-06-25 16:43:31 +0300

    initial commit

---
+++ web/Data/Air_Valves.js
@@ -0,0 +1,10 @@
+var airValve ={
+"type": "FeatureCollection",
+"name": "Air_Valves",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": 24, "subtype": "AV", "diameter": "2", "Status": "Active" }, "geometry": { "type": "Point", "coordinates": [ 37.057564713740057, -1.077431866251502 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 51, "subtype": "AV", "diameter": "6", "Status": "Active" }, "geometry": { "type": "Point", "coordinates": [ 37.072229865678686, -1.089482874862237 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 72, "subtype": "AV", "diameter": "2", "Status": "Active" }, "geometry": { "type": "Point", "coordinates": [ 37.064428623498692, -1.083296543167493 ] } }
+]
+}
---
+++ web/Data/Reducer.js
@@ -0,0 +1,17 @@
+var reducer = {
+"type": "FeatureCollection",
+"name": "Reducer",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": 43, "subtype": "RD", "diameter": null, "Status": "Active" }, "geometry": { "type": "Point", "coordinates": [ 37.06601511212186, -1.087208397292447 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 44, "subtype": "RD", "diameter": null, "Status": "Active" }, "geometry": { "type": "Point", "coordinates": [ 37.077084442735966, -1.089708108762933 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 47, "subtype": "RD", "diameter": null, "Status": "Active" }, "geometry": { "type": "Point", "coordinates": [ 37.082081165009107, -1.095778184163608 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 49, "subtype": "RD", "diameter": null, "Status": "Active" }, "geometry": { "type": "Point", "coordinates": [ 37.087728333351471, -1.098107666539079 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 53, "subtype": "RD", "diameter": null, "Status": "Active" }, "geometry": { "type": "Point", "coordinates": [ 37.078098719609059, -1.094110343251675 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 54, "subtype": "RD", "diameter": null, "Status": "Active" }, "geometry": { "type": "Point", "coordinates": [ 37.080459222186398, -1.09112233324861 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 56, "subtype": "RD", "diameter": "4", "Status": "Active" }, "geometry": { "type": "Point", "coordinates": [ 37.072541044686737, -1.091757910031694 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 57, "subtype": "RD", "diameter": null, "Status": "Active" }, "geometry": { "type": "Point", "coordinates": [ 37.064269477136996, -1.085996411665234 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 58, "subtype": "RD", "diameter": "1", "Status": "Active" }, "geometry": { "type": "Point", "coordinates": [ 37.067511889437149, -1.097035420435006 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": 59, "subtype": "RD", "diameter": null, "Status": "Active" }, "geometry": { "type": "Point", "coordinates": [ 37.07642565356705, -1.087635466382945 ] } }
+]
+}
